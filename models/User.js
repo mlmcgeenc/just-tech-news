01cca3787ad498f570b030ea048aa4826ce25133
Matt McGee <mlmcgeenc@gmail.com>
2022-07-15 11:34:25 -0400

    hash passwords and verify user login

--- models/User.js
+++ models/User.js
@@ -1,21 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create User model
-class User extends Model {}
+class User extends Model {
+	// set up method to run on instance data (per user) to check password
+	checkPassword(loginPw) {
+		return bcrypt.compareSync(loginPw, this.password);
+	}
+}
 
 // define table columns and configuration
 User.init(
 	{
 		// define an id column
 		id: {
-			// use the special Sequelize DataTypes object provide what type of data it is
 			type: DataTypes.INTEGER,
-			// this is the equivalent of SQL's `NOT NULL` option
 			allowNull: false,
-			// instruct that this is the Primary Key
 			primaryKey: true,
-			// turn on auto increment
 			autoIncrement: true,
 		},
 		// define a username column
@@ -27,7 +29,6 @@ User.init(
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
-			// there cannot be any duplicate email values in this table
 			unique: true,
 			// if allowNull is set to false, we can run our data through validators before creating the table data
 			validate: {
@@ -45,6 +46,16 @@ User.init(
 		},
 	},
 	{
+		hooks: {
+			async beforeCreate(newUserData) {
+				newUserData.password = await bcrypt.hash(newUserData.password, 10);
+				return newUserData;
+			},
+			async beforeUpdate(updatedUserData) {
+				updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+				return updatedUserData;
+			},
+		},
 		sequelize,
 		timestamps: false,
 		freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,8 +5,8 @@ const { User } = require('../../models');
 router.get('/', (req, res) => {
 	// Access User model and run .findAll() method
 	User.findAll({
-    attributes: { exclude: ['password'] }
-  })
+		attributes: { exclude: ['password'] },
+	})
 		.then((dbUserData) => res.json(dbUserData))
 		.catch((err) => {
 			console.log(err);
@@ -16,80 +16,104 @@ router.get('/', (req, res) => {
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
-  User.findOne({
-    attributes: { exclude: ['password'] },
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' })
-      return
-    }
-    res.json(dbUserData)
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+	User.findOne({
+		attributes: { exclude: ['password'] },
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbUserData) => {
+			if (!dbUserData) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // POST /api/users
 router.post('/', (req, res) => {
-  // expects {username: 'Lernatino', email" 'lernatino@gmail.com', password: 'password1234'}
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password
-  })
-  .then(dbUserData => res.json(dbUserData))
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+	// expects {username: 'Lernatino', email" 'lernatino@gmail.com', password: 'password1234'}
+	User.create({
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+	})
+		.then((dbUserData) => res.json(dbUserData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+// /login
+router.post('/login', (req, res) => {
+	// expects {email: 'lernatina@gmail.com', password: 'password1234'}
+	User.findOne({
+		where: {
+			email: req.body.email,
+		},
+	}).then((dbUserData) => {
+		if (!dbUserData) {
+			res.status(400).json({ message: 'No user with that email address!' });
+			return;
+		}
+		// Verify User
+		const validPassword = dbUserData.checkPassword(req.body.password);
+
+		if (!validPassword) {
+			res.status(400).json({ message: 'Password is incorrect' });
+			return;
+		}
+		res.json({ user: dbUserData, message: 'You are logged in' });
+	});
 });
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
 	// expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 	// if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
-  User.update(req.body, {
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbUserData => {
-    if (!dbUserData[0]) {
-      res.status(404).json({ message: 'No user found with this id' })
-      return
-    }
-    res.json(dbUserData)
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+	User.update(req.body, {
+		individualHooks: true,
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbUserData) => {
+			if (!dbUserData[0]) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
-  User.destroy({
-    where: {
-      id: req.params.id
-    }
-  })
-  .then(dbUserData => {
-    if (!dbUserData) {
-      res.status(404).json({ message: 'No user found with this id' })
-      return
-    }
-    res.json(dbUserData)
-  })
-  .catch(err => {
-    console.log(err)
-    res.status(500).json(err)
-  })
+	User.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbUserData) => {
+			if (!dbUserData) {
+				res.status(404).json({ message: 'No user found with this id' });
+				return;
+			}
+			res.json(dbUserData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
